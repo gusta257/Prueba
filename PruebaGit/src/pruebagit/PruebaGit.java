fd12a61fe8092ecc709c6ac39d9985ad6c26fac7
Gustavo de Leon <gdeleontobias@gmail.com>
2018-01-14 19:03:43 -0600

    Segundo  cambio

--- PruebaGit/src/pruebagit/PruebaGit.java
+++ PruebaGit/src/pruebagit/PruebaGit.java
@@ -10,12 +10,18 @@ package pruebagit;
  * @author Gustavo
  */
 public class PruebaGit {
+    
+    String nombre;
+    int valor;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+    public String getNombre(){
+        return nombre;
     }
     
 }
